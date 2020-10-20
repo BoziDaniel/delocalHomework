dddfd47dc39d588cd6ed9624bc4a04fb984b5fa5
BoziDaniel <daniel.bozi88@gmail.com>
2020-10-20 12:34:08 +0200

    implemented fetch from api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [palettes, setPalettes] = useState([]);
+  useEffect(() => {
+    const options = {
+      url: "http://www.colourlovers.com/api/palettes/new?format=json",
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    axios(options).then((resp) => {
+      console.log(resp.data);
+      setPalettes(resp.data);
+    });
+  }, []);
   return (
     <div className="App">
-     
+      <h1>ColourLovers.Live</h1>
     </div>
   );
 }
